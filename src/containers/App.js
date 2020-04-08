9e44ed6412031f4d08c64eef9834ffd92689d5fb
shambhavibudholiya <shambhavi20.1998@gmail.com>
2020-04-08 12:37:52 +0530

    redux added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Cardlist from '../components/Cardlist';
 import Searchbox from '../components/Searchbox';
 import './App.css';
 import Scroll from '../components/Scroll';
+import { setSearchField } from "../actions";
 //props never change
 //state is the description of th program, it is an object. It is able to change.
   
+const mapStateToProps = state =>{
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+}
+}
 class App extends React.Component {
     constructor() {
       super()
       this.state = {
-        robots: [],
-        searchfield: ''
+        robots: []
       }  
     }
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users').
-        then(Response => {
-         return Response.json();
-        })
-        .then(users =>this.setState({robots: users}));
-    }
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }   
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(Response => Response.json())
+        .then(users =>{this.setState({robots: users})});
+    }  
     render(){
-        const filterRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-     if (this.state.robots.length ===0){
-         return <h1>Loading</h1>
-     }
-     else{
-    return(
+        const { robots } = this.state;
+        const {searchField, onSearchChange} = this.props;
+        const filterRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })     
+         return !robots.length ?
+         <h1>Loading</h1> :
+          (
             <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
-            <Searchbox searchChange={this.onSearchChange} />
+            <Searchbox searchChange={onSearchChange} />
         <Scroll>
         <Cardlist robots={filterRobots}/>
         </Scroll>
         </div>
         );
     }
-    }
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'; 
 import './index.css';
+import {createStore} from 'redux';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import App from './containers/App';
+import { searchRobots } from './reducers';
+
+
+const store = createStore(searchRobots)
 ReactDOM.render(
+  <Provider store = {store} >
   <App />
+  </Provider>
 ,document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
